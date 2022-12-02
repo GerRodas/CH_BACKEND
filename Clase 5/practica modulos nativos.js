1f12df21df8b4d7c8e2eb41a7fd2d781f630706f
German <germanrodas@gmail.com>
2022-12-02 18:20:14 -0300

    probando modulo crypto

--- Clase 5/practica modulos nativos.js
+++ Clase 5/practica modulos nativos.js
@@ -3,10 +3,29 @@ const crypto = require ('crypto')
 
 const filename = './users.json'
 class UserManager {
-    getUsers = async() =>}
+
+    getUsers = async() => {
     if (fs.existsSync(filename)){
-        const data = await fs.promises.readFile(filename, 'utp-8')
+        const data = await fs.promises.readFile(filename, 'utf-8')
         const users = JSON.parse(data)
         return users
     }
+    return []
 }
+
+creatUser = async(user) => {
+    const user = await this.getUsers()
+
+    user.salt = crypto.randomBytes(128).toString('base64')
+    user.password = crypto.createHmac('sha256', user.salt).update(user.password).digest('hex')
+
+    users.push(user)
+
+    await fs.promises.writeFile(filename, JSON.stringify(users))
+
+    return users
+}
+
+}
+
+console.log()
